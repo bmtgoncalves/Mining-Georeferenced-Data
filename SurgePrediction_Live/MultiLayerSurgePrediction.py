2f6f86dc430356a00a91ca8671d737e3f07f0909
Andreas Mueller <amueller@nyu.edu>
2015-11-10 11:58:51 -0500

    pep8 and python3 compatibility

--- SurgePrediction_Live/MultiLayerSurgePrediction.py
+++ SurgePrediction_Live/MultiLayerSurgePrediction.py
@@ -1,100 +1,105 @@
 import csv
 import numpy as np
 
-lonMin = -74.1 #minimum longitude 
+lonMin = -74.1  # minimum longitude
 lonMax = -73.7
-lonStep = 0.0025 #defines cell size
+lonStep = 0.0025  # defines cell size
 
-latMin = 40.6  #minimum latitude 
+latMin = 40.6  # minimum latitude
 latMax = 41.0
-latStep = 0.0025 #defines cell size
+latStep = 0.0025  # defines cell size
 
-latLen =  int ((latMax - latMin) / latStep) +1   #number of cells on the y-axis
-lonLen =  int ((lonMax - lonMin) / lonStep) +1 #number of cells on the x-axis
+latLen = int((latMax - latMin) / latStep) + 1  # number of cells on the y-axis
+lonLen = int((lonMax - lonMin) / lonStep) + 1  # number of cells on the x-axis
 
-#Cell counts for each source of geo-data
-FSQcellCount = np.zeros ((latLen, lonLen))
-AIRcellCount = np.zeros ((latLen, lonLen))
-TAXIcellCount = np.zeros ((latLen, lonLen))
+# Cell counts for each source of geo-data
+FSQcellCount = np.zeros((latLen, lonLen))
+AIRcellCount = np.zeros((latLen, lonLen))
+TAXIcellCount = np.zeros((latLen, lonLen))
 
 
 ####### LOADING COORDS FROM 3 GEO LAYERS #########
 airbnb_coords = []
 
 with open('listings_sample.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		airbnb_coords.append([float(row['latitude']), float(row['longitude'])])
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        airbnb_coords.append([float(row['latitude']), float(row['longitude'])])
 
-print 'Number of Airbnb listings: ' + str(len(airbnb_coords))
+print('Number of Airbnb listings: ' + str(len(airbnb_coords)))
 
 
 #reading Foursquare venue data #
 foursquare_coords = []
 with open('venue_data_4sq_newyork_anon.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		foursquare_coords.append([float(row['latitude']), float(row['longitude'])])
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        foursquare_coords.append(
+            [float(row['latitude']), float(row['longitude'])])
 
-print 'Number of Foursquare listings: ' + str(len(foursquare_coords))
+print('Number of Foursquare listings: ' + str(len(foursquare_coords)))
 
 #reading Taxi journey coords #
 taxi_coords = []
 with open('taxi_trips.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		taxi_coords.append([float(row['latitude']), float(row['longitude'])])
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        taxi_coords.append([float(row['latitude']), float(row['longitude'])])
 
-print 'Number of Yellow Taxi journeys: ' + str(len(taxi_coords))
+print('Number of Yellow Taxi journeys: ' + str(len(taxi_coords)))
 
 ############ ############ ############ ############
 
 all_coords = [foursquare_coords, airbnb_coords, taxi_coords]
 for i in range(0, 3):
-	current_coords = all_coords[i]
+    current_coords = all_coords[i]
 
-	for coords in current_coords:
-		lat = coords[0]
-		longit = coords[1]
+    for coords in current_coords:
+        lat = coords[0]
+        longit = coords[1]
 
-		#if outside the grid then ingore point
-		if (lat < latMin) or (lat > latMax) or (longit < lonMin) or (longit > lonMax):
-		    continue
+        # if outside the grid then ingore point
+        if (lat < latMin) or (lat > latMax) or (longit < lonMin) or (longit > lonMax):
+            continue
 
-		#if outside the grid then ingore point
-		cx = int ((longit - lonMin) / lonStep)
-		cy = int((lat - latMin) / latStep)
+        # if outside the grid then ingore point
+        cx = int((longit - lonMin) / lonStep)
+        cy = int((lat - latMin) / latStep)
 
-		if i == 0:
-			FSQcellCount[cy, cx] += 1
-		elif i == 1:
-			AIRcellCount[cy, cx] +=1
-		else:
-			TAXIcellCount[cy, cx] +=1
+        if i == 0:
+            FSQcellCount[cy, cx] += 1
+        elif i == 1:
+            AIRcellCount[cy, cx] += 1
+        else:
+            TAXIcellCount[cy, cx] += 1
 
 
-
-#Load Area Average Surge: assume you have queried previously the UBER API and have collected info on pricing for an area
+# Load Area Average Surge: assume you have queried previously the UBER API
+# and have collected info on pricing for an area
 latLongSurge = {}
 for l in open('averageSurgeInCell.csv', 'r'):
-	splits = l.split(',')
-	cy = int(splits[0])
-	cx = int(splits[1])
-	averageSurgeCoeff = float(splits[2])
+    splits = l.split(',')
+    cy = int(splits[0])
+    cx = int(splits[1])
+    averageSurgeCoeff = float(splits[2])
 
-	latLongSurge.setdefault(cy, {})
-	latLongSurge[cy][cx] = averageSurgeCoeff
+    latLongSurge.setdefault(cy, {})
+    latLongSurge[cy][cx] = averageSurgeCoeff
 
 ###################
 import pylab as plt
-surgeValues = [latLongSurge[cy][cx] for cy in range(0, latLen-1) for cx in range(0,lonLen-1) if latLongSurge[cy][cx] !=0]
+surgeValues = [latLongSurge[cy][cx] for cy in range(
+    0, latLen - 1) for cx in range(0, lonLen - 1) if latLongSurge[cy][cx] != 0]
 
-plt.hist(surgeValues , color='yellow', bins=60, label = 'Surge Multipliers',  alpha=0.8)
+plt.hist(surgeValues, color='yellow', bins=60,
+         label='Surge Multipliers', alpha=0.8)
 plt.ylabel('Frequency')
 plt.xlabel('Area Mean Surge Multiplier')
 
-vals = ['1.0','1.05','1.1','1.15','1.2','1.25','1.3','1.35','1.4','1.45']
-plt.xticks([1.0,1.05,1.1,1.15,1.2,1.25,1.3,1.35,1.4,1.45], vals, fontsize=10)
+vals = ['1.0', '1.05', '1.1', '1.15', '1.2',
+        '1.25', '1.3', '1.35', '1.4', '1.45']
+plt.xticks([1.0, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3,
+            1.35, 1.4, 1.45], vals, fontsize=10)
 
 
 plt.grid(True)
@@ -107,53 +112,54 @@ plt.close()
 import scipy.stats as stats
 
 
-yellows = [TAXIcellCount[cy][cx] for cy in range(0, latLen-1) for cx in range(0,lonLen-1) if latLongSurge[cy][cx] !=0]
-places = [FSQcellCount[cy][cx] for cy in range(0, latLen-1) for cx in range(0,lonLen-1) if latLongSurge[cy][cx] !=0]
-listings = [AIRcellCount[cy][cx] for cy in range(0, latLen-1) for cx in range(0,lonLen-1) if latLongSurge[cy][cx] !=0]
+yellows = [TAXIcellCount[cy][cx] for cy in range(
+    0, latLen - 1) for cx in range(0, lonLen - 1) if latLongSurge[cy][cx] != 0]
+places = [FSQcellCount[cy][cx] for cy in range(
+    0, latLen - 1) for cx in range(0, lonLen - 1) if latLongSurge[cy][cx] != 0]
+listings = [AIRcellCount[cy][cx] for cy in range(
+    0, latLen - 1) for cx in range(0, lonLen - 1) if latLongSurge[cy][cx] != 0]
 
 cnt = 0
 labels = ['Yellow Taxis', 'Foursquare Places', 'Airbnb Listings']
-print len(surgeValues)
-print len(yellows)
+print(len(surgeValues))
+print(len(yellows))
 for predictor in [yellows, places, listings]:
-	r, p_value = stats.pearsonr(surgeValues, predictor)
-	print labels[cnt]
-	print r
-	print '---'
-	cnt+=1
+    r, p_value = stats.pearsonr(surgeValues, predictor)
+    print(labels[cnt])
+    print(r)
+    print('---')
+    cnt += 1
 
 ### SUPERVISED LEARNING FOR SURGE PREDICTION###
 
 from sklearn import tree
-y_train = [] #a list for your training labels
-X_train = [] #feature values go here
+y_train = []  # a list for your training labels
+X_train = []  # feature values go here
 for i in range(0, len(surgeValues)):
-	y_train.append(surgeValues[i])
-	X_train.append([yellows[i], listings[i], places[i]])
+    y_train.append(surgeValues[i])
+    X_train.append([yellows[i], listings[i], places[i]])
 
-print 'Training Decision Tree Regressor..'
+print('Training Decision Tree Regressor..')
 ###  Training and Testing: LEAVE ONE OUT ERROR ####
-### Leave one out error has been considered as an unbiased estimator of the generalisation error ###
+# Leave one out error has been considered as an unbiased estimator of the
+# generalisation error ###
 super_predictions = []
 super_predictions2 = []
 for i in range(0, len(surgeValues)):
-	training_data_X = X_train[:i] + X_train[i+1:]
-	label_data_Y = y_train[:i] + y_train[i+1:]
-	NX_train = []
-	ny_train = []
-	for j in range(0, len(label_data_Y)):
-		if label_data_Y[j] == 1.0:
-			continue
-		else:
-			NX_train.append(X_train[j])
-			ny_train.append(y_train[j])
+    training_data_X = X_train[:i] + X_train[i + 1:]
+    label_data_Y = y_train[:i] + y_train[i + 1:]
+    NX_train = []
+    ny_train = []
+    for j in range(0, len(label_data_Y)):
+        if label_data_Y[j] == 1.0:
+            continue
+        else:
+            NX_train.append(X_train[j])
+            ny_train.append(y_train[j])
 
-	clf = tree.DecisionTreeRegressor(max_depth=30).fit(NX_train, ny_train)
-	super_predictions.append(clf.predict(X_train[i])[0])
+    clf = tree.DecisionTreeRegressor(max_depth=30).fit(NX_train, ny_train)
+    super_predictions.append(clf.predict(X_train[i])[0])
 
 
 r, p_value = stats.pearsonr(surgeValues, super_predictions)
-print 'Decision Tree Supervised Learning Regressor, r :'  + str(r)
-
-
-
+print('Decision Tree Supervised Learning Regressor, r :' + str(r))
--- SurgePrediction_Live/UberQuest.py
+++ SurgePrediction_Live/UberQuest.py
@@ -1,68 +1,64 @@
 import csv
 import requests
 import time
-import itertools
 import random
 
+
 def uber_request(latitude, longitude, code='price', end_latitude=None, end_longitude=None):
-	client_id = 'ADD'
-	server_token = 'ADD'
-	secret = 'ADD'
+    client_id = 'ADD'
+    server_token = 'ADD'
+    secret = 'ADD'
 
+    price_parameters = {
+        # 'client_id': client_id,
+        'server_token': server_token,
+        'client_secret': secret,
+        'start_latitude': latitude,
+        'start_longitude': longitude,
+        'end_latitude': end_latitude,
+        'end_longitude': end_longitude,
+    }
 
-	price_parameters = {
-		# 'client_id': client_id,
-	    'server_token': server_token,
-	    'client_secret': secret,
-	    'start_latitude': latitude,
-	    'start_longitude': longitude,
-	    'end_latitude': end_latitude, 
-	    'end_longitude': end_longitude,
-	}
-
-	price_url = 'https://api.uber.com/v1/estimates/price'
-	print 'Getting price quote...'
-	req_url = price_url
-	response_price = requests.get(price_url, params=price_parameters)
-	data_price = response_price.json()
-	return data_price
+    price_url = 'https://api.uber.com/v1/estimates/price'
+    print('Getting price quote...')
+    req_url = price_url
+    response_price = requests.get(price_url, params=price_parameters)
+    data_price = response_price.json()
+    return data_price
 
 
 #reading Taxi journey coords #
 taxi_coords = []
 with open('taxi_trips.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		taxi_coords.append([float(row['latitude']), float(row['longitude'])])
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        taxi_coords.append([float(row['latitude']), float(row['longitude'])])
 
-print 'Number of Yellow Taxi journeys: ' + str(len(taxi_coords))
+print('Number of Yellow Taxi journeys: ' + str(len(taxi_coords)))
 
 f_out = open('UberQuestOutput.txt', 'w')
 
-for i in range(0,1000):
-	coordsOrigin = random.choice(taxi_coords)
-	coordsDestination = random.choice(taxi_coords)
-	latitude_O = coordsOrigin[0]
-	longitude_O = coordsOrigin[1]
+for i in range(0, 1000):
+    coordsOrigin = random.choice(taxi_coords)
+    coordsDestination = random.choice(taxi_coords)
+    latitude_O = coordsOrigin[0]
+    longitude_O = coordsOrigin[1]
 
-	latitude_D = coordsDestination[0]
-	longitude_D = coordsDestination[1]
+    latitude_D = coordsDestination[0]
+    longitude_D = coordsDestination[1]
 
-	try:
-		uber_response = uber_request(latitude_O, longitude_O, 'price', latitude_D, longitude_D)
-	except:
-		time.sleep(10.0)
-		continue
-	print >> f_out, str((latitude_O, longitude_O, uber_response, latitude_D, longitude_D))
-	print 'Query retrieved, sleeping for a sec.'
-	time.sleep(1.0)
+    try:
+        uber_response = uber_request(
+            latitude_O, longitude_O, 'price', latitude_D, longitude_D)
+    except:
+        time.sleep(10.0)
+        continue
+    print >> f_out, str(
+        (latitude_O, longitude_O, uber_response, latitude_D, longitude_D))
+    print('Query retrieved, sleeping for a sec.')
+    time.sleep(1.0)
 
 
 f_out.close()
 
-print 'Uber Quest is Over.'
-
-
-
-
-
+print('Uber Quest is Over.')
